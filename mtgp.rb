05af9dc2ced0b2d566b9c7dafc96099a5353be8c
trescenzi <trescenzi@gmail.com>
2014-03-29 02:14:00 +0000

    fixed the formatting and integrated in the api

--- mtgp.rb
+++ mtgp.rb
@@ -1,13 +1,24 @@
 require "rubygems"
 require "sinatra/base"
 require 'haml'
+require 'net/http'
+require 'json'
 
 
 class Mtgp < Sinatra::Base
 set :bind, '0.0.0.0'
 set :haml, :format => :html5
   get '/' do
-    haml :index
+    cname = params[:cardname]
+    unless cname.nil? 
+      uri = URI.parse("http://magictcgprices.appspot.com/api/tcgplayer/price.json?cardname="+URI.escape(cname))
+      res = Net::HTTP.get_response(uri)
+      prices = JSON.parse(res.body)
+    end
+    if prices.nil?
+      prices = ["", "", ""]
+    end
+    haml :index, :locals => {:low => prices[0], :mid => prices[1], :high => prices[2]}
   end
 
   get '/search' do
